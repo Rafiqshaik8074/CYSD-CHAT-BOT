5b781bd068ca70ef808fc70e001baed3581f1600
Rafiqshaik8074 <shaikrafiq8074@gmail.com>
2025-06-27 15:06:13 +0530

    Updated deployment changes

--- cysd_chatbot/settings.py
+++ cysd_chatbot/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-hypoa-@iqtn)b8yz5!xja6(by!=@wm)=-^@qvxn$#icbh)^*t)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
 
 
 # Application definition
