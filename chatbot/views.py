d12dde95a7b305cee42cd86f551f57f191debb13
Rafiqshaik8074 <shaikrafiq8074@gmail.com>
2025-06-30 12:21:54 +0530

    Updated Error response

--- chatbot/views.py
+++ chatbot/views.py
@@ -11,9 +11,23 @@ def chat_view(request):
     print('Hello world'.center(100, '-'))
     return render(request, "chatbot/Chat3.html")
 
+# def get_response(request):
+#     user_query = request.GET.get("message")
+#     if user_query:
+#         bot_reply = generate_response(user_query)
+#         return JsonResponse({"reply": bot_reply})
+#     return JsonResponse({"reply": "Please ask something."})
+
+
+
 def get_response(request):
-    user_query = request.GET.get("message")
-    if user_query:
-        bot_reply = generate_response(user_query)
-        return JsonResponse({"reply": bot_reply})
-    return JsonResponse({"reply": "Please ask something."})
+    try:
+        user_query = request.GET.get('message', '')
+        print('Message from frontend: ', user_query)
+        if(user_query):
+            bot_reply = generate_response(user_query)
+            print('Response from ChatBot: ', bot_reply)
+            return JsonResponse({'reply': bot_reply})
+    except Exception as e:
+        print(e)
+        return JsonResponse({'error': str(e)}, status=500)
